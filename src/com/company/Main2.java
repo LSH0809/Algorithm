912f7edd88b174bd1467d2781fd18b95a8ef1882
LSH0809 <skyey94@naver.com>
2021-07-05 17:32:37 +0900

    init commit

---
+++ src/com/company/Main2.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main2 {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("hello world");
+    }
+}
---
+++ src/com/company2/Main3.java
@@ -0,0 +1,7 @@
+package com.company2;
+
+public class Main3 {
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
