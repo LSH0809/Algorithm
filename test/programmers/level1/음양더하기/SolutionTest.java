9095b9b4779645c311b57adac442cd570ad56b43
LSH0809 <skyey94@naver.com>
2021-07-10 01:10:18 +0900

    음양 더하기 commit

---
+++ test/programmers/level1/음양더하기/SolutionTest.java
@@ -0,0 +1,19 @@
+package programmers.level1.음양더하기;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+    @Test
+    public void solution() {
+        Solution solution = new Solution();
+        int[] absolutes1 = {4, 7, 12}, absolutes2 = {1, 2, 3};
+        boolean[] signs1 = {true, false, true}, signs2 = {false, false, true};
+
+        Assert.assertEquals(solution.solution(absolutes1, signs1), 9);
+        Assert.assertEquals(solution.solution(absolutes2, signs2), 0);
+    }
+}
