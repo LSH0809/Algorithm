4bb7548e1bee8de8e66e502b4e8201f34518af8e
LSH0809 <skyey94@naver.com>
2021-07-10 23:08:26 +0900

    이진 변환 반복하기 commit

---
+++ src/programmers/level2/이진변환반복하기/Solution.java
@@ -0,0 +1,23 @@
+package programmers.level2.이진변환반복하기;
+
+public class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[2];
+        int try_count = 0;
+        int zero_count = 0;
+
+        while (!s.equals("1")) {
+            try_count++;
+            String temp = s.replaceAll("0", "");
+            int zero_temp = 0;
+            zero_temp = s.length() - temp.length();
+            zero_count += zero_temp;
+
+            s = Integer.toBinaryString(temp.length());
+        }
+        answer[0] = try_count;
+        answer[1] = zero_count;
+
+        return answer;
+    }
+}
