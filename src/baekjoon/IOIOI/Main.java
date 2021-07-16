cea90cd46266b3105e970afded667a9526c69318
LSH0809 <skyey94@naver.com>
2021-07-16 22:38:23 +0900

    Baekjoon 1743번 음식물 피하기 commit

--- src/baekjoon/IOIOI/Main.java
+++ src/baekjoon/IOIOI/Main.java
@@ -3,6 +3,7 @@ package baekjoon.IOIOI;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 class Main {
     public static void main(String[] args) throws IOException {
@@ -13,8 +14,7 @@ class Main {
         int answer = 0;
         int[] dp = new int[M];
 
-        dp[0] = 0;
-        dp[1] = 0;
+        Arrays.fill(dp, 0);
 
         for (int i = 2; i < M; i++) {
             String temp = str.substring(i - 2, i + 1);
