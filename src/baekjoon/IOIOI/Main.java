7ab10bbca9a16dae952d7b8bec181ad214539824
LSH0809 <skyey94@naver.com>
2021-07-16 21:28:22 +0900

    Baekjoon 5525 IOIOI commit

---
+++ src/baekjoon/IOIOI/Main.java
@@ -0,0 +1,30 @@
+package baekjoon.IOIOI;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine());
+        int M = Integer.parseInt(bufferedReader.readLine());
+        String str = bufferedReader.readLine();
+        int answer = 0;
+        int[] dp = new int[M];
+
+        dp[0] = 0;
+        dp[1] = 0;
+
+        for (int i = 2; i < M; i++) {
+            String temp = str.substring(i - 2, i + 1);
+            if (temp.equals("IOI")) {
+                dp[i] = dp[i - 2] + 1;
+            }
+            if (dp[i] >= N) {
+                answer++;
+            }
+        }
+        System.out.println(answer);
+    }
+}
