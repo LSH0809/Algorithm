7e76b11da8cf6b340a877f448a8a0be828f1a07f
LSH0809 <skyey94@naver.com>
2021-07-17 23:21:51 +0900

    프로그래머스 기능 개발 commit

--- src/programmers/level2/기능개발/Solution.java
+++ src/programmers/level2/기능개발/Solution.java
@@ -1,52 +1,52 @@
-//package programmers.level2.기능개발;
-//
-//import java.util.ArrayList;
-//import java.util.LinkedList;
-//import java.util.List;
-//import java.util.Queue;
-//
-//public class Solution {
-//    public int[] solution(int[] progresses, int[] speeds) {
-//        List<Integer> answer = new ArrayList<>();
-//        Queue<Integer> queue1 = new LinkedList<>();
-//        Queue<Integer> queue2 = new LinkedList<>();
-//        int n = 0;
-//
-//        for (int i = 0; i < progresses.length; i++) {
-//            queue1.offer(progresses[i]);
-//            queue2.offer(speeds[i]);
-//        }
-//
-//        while (!queue1.isEmpty()) {
-//            n++;
-//            int temp = check(n, queue1, queue2);
-//            if (temp != 0) {
-//                answer.add(temp);
-//            }
-//        }
-//
-//        int[] result = new int[answer.size()];
-//
-//        for (int i = 0; i < result.length; i++) {
-//            result[i] = answer.get(i);
-//        }
-//
-//        return result;
-//    }
-//
-//    public static int check(int n, Queue<Integer> queue1, Queue<Integer> queue2) {
-//        int count = 0;
-//
-//        while (!queue1.isEmpty()) {
-//            if ((queue1.peek() + queue2.peek() * n) >= 100) {
-//                queue1.poll();
-//                queue2.poll();
-//                count++;
-//            } else {
-//                break;
-//            }
-//        }
-//
-//        return count;
-//    }
-//}
+package programmers.level2.기능개발;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        List<Integer> answer = new ArrayList<>();
+        Queue<Integer> queue1 = new LinkedList<>();
+        Queue<Integer> queue2 = new LinkedList<>();
+        int n = 0;
+
+        for (int i = 0; i < progresses.length; i++) {
+            queue1.offer(progresses[i]);
+            queue2.offer(speeds[i]);
+        }
+
+        while (!queue1.isEmpty()) {
+            n++;
+            int temp = check(n, queue1, queue2);
+            if (temp != 0) {
+                answer.add(temp);
+            }
+        }
+
+        int[] result = new int[answer.size()];
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = answer.get(i);
+        }
+
+        return result;
+    }
+
+    public static int check(int n, Queue<Integer> queue1, Queue<Integer> queue2) {
+        int count = 0;
+
+        while (!queue1.isEmpty()) {
+            if ((queue1.peek() + queue2.peek() * n) >= 100) {
+                queue1.poll();
+                queue2.poll();
+                count++;
+            } else {
+                break;
+            }
+        }
+
+        return count;
+    }
+}
