5a41b32f7ca304e1946721c3c95e681ee41d83d3
LSH0809 <skyey94@naver.com>
2021-07-28 23:12:54 +0900

    Baekjoon_11719 그대로 출력하기 2 commit

---
+++ src/baekjoon/Baekjoon_11719/Main.java
@@ -0,0 +1,15 @@
+package baekjoon.Baekjoon_11719;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String line = null;
+        while (sc.hasNextLine()) {
+            line = sc.nextLine();
+            System.out.println(line);
+        }
+    }
+}
