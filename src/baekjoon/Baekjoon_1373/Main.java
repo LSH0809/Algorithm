1df793f4c8a5367a68759bae792489edee583c02
LSH0809 <skyey94@naver.com>
2021-07-29 22:41:54 +0900

    Baekjoon_1373 2진수 8진수 README commit

--- src/baekjoon/Baekjoon_1373/Main.java
+++ src/baekjoon/Baekjoon_1373/Main.java
@@ -16,8 +16,8 @@ public class Main {
         } else if (len % 3 == 2) {
             System.out.print((input.charAt(0) - '0') * 2 + input.charAt(1) - '0');
         }
-        for(int i = len % 3; i < len; i+=3){
-            System.out.print((input.charAt(i) - '0')*4 + (input.charAt(i + 1) - '0') * 2 + (input.charAt(i+2) - '0'));
+        for (int i = len % 3; i < len; i += 3) {
+            System.out.print((input.charAt(i) - '0') * 4 + (input.charAt(i + 1) - '0') * 2 + (input.charAt(i + 2) - '0'));
         }
     }
 }
