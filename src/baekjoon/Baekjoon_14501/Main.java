ee57eef093fd73f50ae01642c5947a760cd17421
LSH0809 <skyey94@naver.com>
2021-08-05 21:29:04 +0900

    Baekjoon_20365 블로그2 풀이 commit

---
+++ src/baekjoon/Baekjoon_14501/Main.java
@@ -0,0 +1,34 @@
+package baekjoon.Baekjoon_14501;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bf.readLine());
+        int[] T = new int[N + 5];
+        int[] P = new int[N + 5];
+        int[] dp = new int[N + 5];
+
+        for (int i = 0; i < N; i++) {
+            String line = bf.readLine();
+            StringTokenizer st = new StringTokenizer(line, " ");
+            T[i] = Integer.parseInt(st.nextToken());
+            P[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (i + T[i] <= N) {
+                dp[i + T[i]] = Math.max(dp[i + T[i]], dp[i] + P[i]);
+            }
+
+            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
+        }
+
+        System.out.println(dp[N]);
+    }
+}
