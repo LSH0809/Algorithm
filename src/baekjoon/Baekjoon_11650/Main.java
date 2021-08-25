d1fcf9bc8f75b49a360371bb5a1fe356864075b1
LSH0809 <skyey94@naver.com>
2021-08-25 20:49:56 +0900

    Baekjoon_11650 좌표 정렬하기 풀이 commit

--- src/baekjoon/Baekjoon_11650/Main.java
+++ src/baekjoon/Baekjoon_11650/Main.java
@@ -1,7 +1,6 @@
 package baekjoon.Baekjoon_11650;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
@@ -17,14 +16,11 @@ public class Main {
             arr[i][1] = scanner.nextInt();
         }
 
-        Arrays.sort(arr, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if (o1[0] == o2[0])
-                    return o1[1] - o2[1];
-                else
-                    return o1[0] - o2[0];
-            }
+        Arrays.sort(arr, (o1, o2) -> {
+            if (o1[0] == o2[0])
+                return o1[1] - o2[1];
+            else
+                return o1[0] - o2[0];
         });
 
         for (int i = 0; i < N; i++) {
