c3b594f08b097360be320b7bcd089c9e91ede97b
LSH0809 <skyey94@naver.com>
2021-09-04 22:01:13 +0900

    Baekjoon_5052 전화번호 목록 풀이 commit

---
+++ src/baekjoon/Baekjoon_5052/Main.java
@@ -0,0 +1,37 @@
+package baekjoon.Baekjoon_5052;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        String[] arr;
+        boolean flag;
+        while (T-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            arr = new String[n];
+            flag = true;
+
+            for (int i = 0; i < n; i++) {
+                arr[i] = br.readLine();
+            }
+
+            Arrays.sort(arr);
+
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i + 1].startsWith(arr[i])) {
+                    flag = false;
+                }
+            }
+
+            if (flag)
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+        }
+    }
+}
