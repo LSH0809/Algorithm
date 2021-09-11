1b360d076371d09059eabc2978031b9313763acf
LSH0809 <skyey94@naver.com>
2021-09-11 22:05:33 +0900

    Baekjoon_2548 대표 자연수 풀이 commit

---
+++ src/baekjoon/Baekjoon_2548/Main.java
@@ -0,0 +1,37 @@
+package baekjoon.Baekjoon_2548;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int solve(int[] arr) {
+        if (arr.length == 1)
+            return arr[0];
+
+        if (arr.length % 2 == 0) {
+            return arr[arr.length / 2 - 1];
+        } else {
+            return arr[arr.length / 2 + 1];
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int answer = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        answer = solve(arr);
+        System.out.println(answer);
+    }
+}
