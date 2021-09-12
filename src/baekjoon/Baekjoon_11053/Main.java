7d6f81d4dff86258b67e7209e5d101f5f31d4ee8
LSH0809 <skyey94@naver.com>
2021-09-12 18:59:39 +0900

    Baekjoon_11053 가장 긴증가하는 부분 수열 풀이 commit

---
+++ src/baekjoon/Baekjoon_11053/Main.java
@@ -0,0 +1,39 @@
+package baekjoon.Baekjoon_11053;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] dp = new int[N];
+        int max = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.fill(dp,1);
+        
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(max);
+    }
+}
