790a5ecf8a0243672c7f1a3afb540b8a1d6c2e77
LSH0809 <skyey94@naver.com>
2021-09-29 22:17:04 +0900

    Baekjoon_11000 강의실 배정 풀이

---
+++ src/baekjoon/Baekjoon_11000/Main.java
@@ -0,0 +1,44 @@
+package baekjoon.Baekjoon_11000;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static class Schedule{
+        int s,t;
+
+        public Schedule(int s, int t) {
+            this.s = s;
+            this.t = t;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int answer = 0;
+        Schedule[] schedules = new Schedule[N];
+        for(int i =0 ; i< N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+            schedules[i] = new Schedule(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
+        }
+        Arrays.sort(schedules, new Comparator<Schedule>() {
+            @Override
+            public int compare(Schedule o1, Schedule o2) {
+                return o1.s == o2.s ? o1.t - o2.t : o1.s - o2.s;
+            }
+        });
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        priorityQueue.offer(schedules[0].t);
+
+        for(int i = 1; i< N; i++){
+            if(priorityQueue.peek() <= schedules[i].s){
+                priorityQueue.poll();
+            }
+            priorityQueue.add(schedules[i].t);
+        }
+        answer = priorityQueue.size();
+        System.out.println(answer);
+    }
+}
