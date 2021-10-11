4d2dfe648d8a57f21420288d96e43ae892e9e49c
LSH0809 <skyey94@naver.com>
2021-10-11 23:41:23 +0900

    Baekjoon_11724 연결 요소의 개수 풀이

--- src/baekjoon/Baekjoon_11724/Main.java
+++ src/baekjoon/Baekjoon_11724/Main.java
@@ -6,46 +6,44 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class Main {
-    static boolean[] visited;
     static int[][] arr;
-    static int N, M;
+    static boolean[] visited;
+    static int N, M, result;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String s = bufferedReader.readLine();
-        StringTokenizer st = new StringTokenizer(s, " ");
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        arr = new int[N + 1][N + 1];
-
-        for (int i = 0; i < M; i++) {
-            String line = bufferedReader.readLine();
-            StringTokenizer str = new StringTokenizer(line, " ");
-            int a = Integer.parseInt(str.nextToken());
-            int b = Integer.parseInt(str.nextToken());
-
-            arr[a][b] = 1;
-            arr[b][a] = 1;
+    public static void solve(int idx) {
+        if (visited[idx])
+            return;
+        visited[idx] = true;
+        for (int i = 1; i <= N; i++) {
+            if (arr[idx][i] == 1)
+                solve(i);
         }
-        int answer = 0;
-        visited = new boolean[N + 1];
-
-        for (int i = 1; i < N + 1; i++) {
-            if (visited[i] == false) {
-                dfs(i);
-                answer++;
-            }
-        }
-        System.out.println(answer);
     }
 
-    public static void dfs(int start) {
-        visited[start] = true;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        int x, y;
+        result = 0;
+        arr = new int[N + 1][N + 1];
+        visited = new boolean[N + 1];
+        for (int i = 1; i <= M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
 
-        for (int i = 1; i < N + 1; i++) {
-            if (arr[start][i] == 1 && visited[i] == false) {
-                dfs(i);
+            arr[x][y] = 1;
+            arr[y][x] = 1;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                solve(i);
+                result += 1;
             }
         }
+        System.out.println(result);
     }
 }
