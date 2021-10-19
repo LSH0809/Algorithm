4fcff49b8781a041d9663a21cf74b4549ee3dc24
LSH0809 <skyey94@naver.com>
2021-10-19 21:39:49 +0900

    Baekjoon_10819 차이를 최대로 풀이

---
+++ src/baekjoon/Baekjoon_10819/Main.java
@@ -0,0 +1,45 @@
+package baekjoon.Baekjoon_10819;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int maxValue = Integer.MIN_VALUE;
+    static boolean[] visited;
+    static int[] newArr, arr;
+
+    public static void solve(int depth, int N, int[] arr) {
+        int sum = 0;
+        if (depth == N) {
+            for (int i = 0; i < N - 1; i++) {
+                sum += Math.abs(newArr[i + 1] - newArr[i]);
+            }
+            maxValue = Math.max(maxValue, sum);
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                newArr[depth] = arr[i];
+                solve(depth + 1, N, arr);
+                visited[i] = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        newArr = new int[N];
+        visited = new boolean[N];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        solve(0, N, arr);
+        System.out.println(maxValue);
+    }
+}
