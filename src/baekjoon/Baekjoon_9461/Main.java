86298b7262de07583f2e4b535ef7f682a44362d5
LSH0809 <skyey94@naver.com>
2022-01-03 21:19:50 +0900

    Baekjoon_9461 파도반 수열 풀이

---
+++ src/baekjoon/Baekjoon_9461/Main.java
@@ -0,0 +1,27 @@
+package baekjoon.Baekjoon_9461;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		long[] dp = new long[101];
+		dp[0] = 0;
+		dp[1] = 1;
+		dp[2] = 1;
+		dp[3] = 1;
+		
+		for(int i = 4; i< 101; i++){
+			dp[i] = dp[i-2] + dp[i-3];
+		}
+		
+		while(t-- > 0){
+			int n = Integer.parseInt(br.readLine());
+			System.out.println(dp[n]);
+		}
+	}
+}
