c5b022555ae88ffb5c91668468f9cc5e3701fe7d
unknown <skyey94@naver.com>
2022-03-11 23:55:16 +0900

    Baekjoon_2805 나무 자르기 풀이

--- src/baekjoon/Baekjoon_2805/Main.java
+++ src/baekjoon/Baekjoon_2805/Main.java
@@ -1,49 +1,55 @@
 package baekjoon.Baekjoon_2805;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] arr;
+    static long[] arr;
+    static int n;
+    static long m;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
 
-        String input_line = br.readLine();
+        n = Integer.parseInt(st.nextToken());
+        m = Long.parseLong(st.nextToken());
 
-        st = new StringTokenizer(input_line, " ");
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-
-        arr = new int[N];
-
-        String arr_line = br.readLine();
-        st = new StringTokenizer(arr_line, " ");
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        arr = new long[n];
+        st = new StringTokenizer(br.readLine()," ");
+        for(int i = 0; i< n; i++){
+            arr[i] = Long.parseLong(st.nextToken());
         }
 
-        int start = 0;
-        int end = (int) 1e9;
-        int result = 0;
+        Arrays.sort(arr);
 
-        while (start <= end) {
-            long sum = 0;
-            int mid = (start + end) / 2;
-            for (int i = 0; i < N; i++) {
-                if (arr[i] > mid)
-                    sum += arr[i] - mid;
+        bw.write(binarySearch(1,arr[n-1]));
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+
+    private static String binarySearch(long start, long end){
+        long sum = 0;
+        while(start <= end){
+            sum = 0;
+            long mid = (start + end) / 2;
+            for(int i = 0; i< arr.length; i++){
+                if(arr[i] > mid) {
+                    sum += (arr[i] - mid);
+                }
             }
-            if (sum < M) {
+
+            if(sum < m){
                 end = mid - 1;
-            } else {
-                result = mid;
+            }else {
                 start = mid + 1;
             }
         }
-        System.out.println(result);
+
+        return String.valueOf(end);
     }
 }
