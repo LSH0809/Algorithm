aecd877dffafb5ad4b6e8632c8097d4e13d55362
unknown <skyey94@naver.com>
2022-03-12 22:27:21 +0900

    Baekjoon_2110 공유기 설치 풀이

--- src/baekjoon/Baekjoon_2110/Main.java
+++ src/baekjoon/Baekjoon_2110/Main.java
@@ -1,47 +1,60 @@
 package baekjoon.Baekjoon_2110;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int[] arr;
+    static int n,c;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        int N = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
-        int[] arr = new int[N];
-        for(int i = 0; i< N; i++){
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
         Arrays.sort(arr);
 
-        int start = 1;
-        int answer = 0;
-        int end = arr[N-1] - arr[0];
+        bw.write(binarySearch(1, arr[arr.length - 1]));
+        bw.flush();
 
+        bw.close();
+        br.close();
+    }
+
+    private static String binarySearch(int start, int end){
+        int answer = 0;
         while(start <= end){
             int mid = (start + end) / 2;
-            int count = 1;
-            int left = arr[0];
 
-            for(int i = 1; i< N; i++){
-                if(arr[i] - left >= mid){
-                    count += 1;
-                    left = arr[i];
-                }
-            }
-
-            if(count >= C){
+            if(canInstall(mid) < c){
+                end = mid - 1;
+            }else{
                 answer = mid;
                 start = mid + 1;
-            }else{
-                end = mid - 1;
             }
         }
-        System.out.println(answer);
+        return String.valueOf(answer);
+    }
+
+    private static int canInstall(int dist){
+        int count = 1; // 시작 집은 무조건 설치
+        int lastInstall = arr[0];
+
+        for(int i = 1; i< arr.length; i++){
+            int toLocate = arr[i];
+
+            if(toLocate - lastInstall >= dist){
+                count += 1;
+                lastInstall = toLocate;
+            }
+        }
+
+        return count;
     }
 }
